40b65a54f06b58225e18faa9c9c88d1bd74d3ba7
joeymasip <joey.masip@gmail.com>
2019-01-21 20:35:19 +0100

    -api auth controller for registering new users

---
+++ src/Controller/Api/ApiAuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller\Api;
+
+use FOS\UserBundle\Model\UserManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\User;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/auth")
+ */
+class ApiAuthController extends AbstractController
+{
+    /**
+     * @Route("/register", name="api_auth_register",  methods={"POST"})
+     * @param Request $request
+     * @param UserManagerInterface $userManager
+     * @return JsonResponse|\Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function register(Request $request, UserManagerInterface $userManager)
+    {
+        $data = json_decode(
+            $request->getContent(),
+            true
+        );
+
+        $username = $data['username'];
+        $password = $data['password'];
+        $email = $data['email'];
+
+        $user = new User();
+
+        $user
+            ->setUsername($username)
+            ->setPlainPassword($password)
+            ->setEmail($email)
+            ->setEnabled(true)
+            ->setRoles(['ROLE_USER'])
+            ->setSuperAdmin(false)
+        ;
+
+        try {
+            $userManager->updateUser($user, true);
+        } catch (\Exception $e) {
+            return new JsonResponse(["error" => $e->getMessage()], 500);
+        }
+
+        return new JsonResponse(["success" => $user->getUsername(). " has been registered!"], 200);
+    }
+}
